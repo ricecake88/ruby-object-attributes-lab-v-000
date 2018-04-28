63aef9755cc165507b9ba223d3078a98a6b39d0f
Grace Shih <vocalise@gmail.com>
2018-04-28 20:15:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,19 @@
 class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
   def setName=(name)
     @name = name
+  end
+  
+  def getName
+    @name
+  end
+  
+  def setBreed=(breed)
+    @breed=breed
   end
 end
 
